b992806695e7a41ec0394e7e4f973796a78c1966
DESKTOP-AT770DD\Dell <mahesh.raj@minddeft.com>
2023-02-03 16:27:34 +0530

    wallet add

--- src/Contractfunction.js
+++ src/Contractfunction.js
@@ -1,7 +1,146 @@
+import { ethers } from "ethers";
 import React from "react";
-
+import { useState } from "react";
+import { useEffect, useCallback } from "react";
+import Web3Modal from "web3modal";
+import Header from "./Header";
+let web3Modal = new Web3Modal({
+  network: "testnet",
+  cacheProvider: true,
+  providerOptions: {},
+});
 const Contractfunction = () => {
-  return <div>Contractfunction</div>;
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState();
+  const [account, setAccount] = useState(null);
+  const [error, setError] = useState("");
+  const [chainId, setChainId] = useState();
+  const [isConnect, setConnect] = useState(false);
+  const [balance, setBalance] = useState("-");
+
+  const connectWallet = async () => {
+    try {
+      const provider = await web3Modal.connect("walletconnect");
+      const library = new ethers.providers.Web3Provider(provider);
+      const signer = await library.getSigner();
+      localStorage.setItem("modalProvider", 1);
+      const accounts = await library.listAccounts();
+      const network = await library.getNetwork();
+      let accBalance = await signer.getBalance();
+      accBalance = ethers.utils.formatEther(accBalance);
+      setBalance(accBalance);
+      setProvider(provider);
+      setSigner(signer);
+      if (accounts) {
+        let account = await Web3.utils.toChecksumAddress(accounts[0]);
+        setAccount(account);
+        setConnect(true);
+      }
+      setChainId(network.chainId);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const refreshState = async () => {
+    setAccount();
+    setChainId();
+    setBalance();
+    // setNetwork("");
+  };
+  useEffect(() => {
+    (async () => {
+      if (localStorage.getItem("modalProvider")) await connectWallet();
+    })();
+  }, []);
+  const disConnectWallet = async () => {
+    await web3Modal.clearCachedProvider();
+    localStorage.removeItem("modalProvider");
+    refreshState();
+    setConnect(false);
+  };
+  useEffect(() => {
+    (async () => {
+      let data = [];
+      if (chainId !== 5 && isConnect) {
+        data = [
+          {
+            chainId: "0x5",
+            chainName: "Goerli test network",
+            nativeCurrency: {
+              name: "ETH",
+              symbol: "GoerliETH",
+              decimals: 18,
+            },
+            rpcUrls: ["https://goerli.infura.io/v3/"],
+            blockExplorerUrls: ["https://goerli.etherscan.io"],
+          },
+        ];
+        console.log("chainId", chainId);
+
+        try {
+          await window.ethereum
+            .request({
+              method: "wallet_addEthereumChain",
+              params: data,
+            })
+            .then((resp) => {
+              console.log("resp", resp);
+              connectWallet();
+            });
+        } catch {
+          console.log("catched");
+        }
+      }
+    })();
+  }, [chainId]);
+
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = async (accounts) => {
+        connectWallet();
+        await getList();
+        // console.log("accountsChanged", accounts);
+
+        if (accounts) {
+          let account = await Web3.utils.toChecksumAddress(accounts[0]);
+          setAccount(account);
+        }
+      };
+
+      const handleChainChanged = (_hexChainId) => {
+        setChainId(_hexChainId);
+      };
+
+      const handleDisconnect = () => {
+        // console.log("disconnect", error);
+        disConnectWallet();
+      };
+
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
+
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
+        }
+      };
+    }
+  }, [provider]);
+  return (
+    <div>
+      <Header
+        isConnect={isConnect}
+        // balance={balance}
+        account={account}
+        connectWallet={connectWallet}
+        disConnectWallet={disConnectWallet}
+      />
+    </div>
+  );
 };
 
 export default Contractfunction;
